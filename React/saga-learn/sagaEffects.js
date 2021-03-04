c3223b2fef05fb2b4ae23584348bdcbdeef40fe3
Deen John <dj.itbhu@gmail.com>
2021-03-04 21:20:22 +0530

    saga fetch

--- React/saga-learn/sagaEffects.js
+++ React/saga-learn/sagaEffects.js
@@ -1,9 +1,24 @@
+//https://stackoverflow.com/questions/47909392/best-way-to-listen-for-actions-in-saga-whiletrue-take-vs-whiletake
+
 const { createStore, applyMiddleware } =require('redux')
 const createSagaMiddleware =require('redux-saga').default
-const { takeEvery ,take,fork}=require('redux-saga/effects') 
-const {delay} =require('redux-saga')
+const { takeEvery ,take,fork,put}=require('redux-saga/effects') 
+const {delay} =require('redux-saga');
+ require('isomorphic-fetch');
+
+function wait(ms) {
+  var start = Date.now(),
+      now = start;
+  while (now - start < ms) {
+    now = Date.now();
+  }
+}
+
 const sagaMiddleware = createSagaMiddleware()
-const reducer=(state=[],action)=>{return [...state,action.type];}
+const reducer=(state=[],action)=>{
+  ////console.log('state ',state)
+  return [...state,action.type];
+}
 const store = createStore(
     reducer,
     applyMiddleware(sagaMiddleware)
@@ -12,17 +27,17 @@ function* takeSaga() {
   while(true){
     const action=yield take('testTake')
     console.log(action)
-    yield delay(1000)
+    yield delay(1000) // sleep , no new thread
   }
 }
 
 function* takeEverySaga() {
     yield takeEvery('testTakeEvery',function* (action){
         console.log(action)
-        yield delay(1000)
+        yield delay(1000) // even though sleep , it spawns another thread
     })
 }
-
+ 
 function* takeSagaWithFork() {
     while(true){
       const action=yield take('testTakeWithFork')
@@ -33,17 +48,34 @@ function* takeSagaWithFork() {
     }
 }
 
-sagaMiddleware.run(takeSaga)
-sagaMiddleware.run(takeEverySaga)
-sagaMiddleware.run(takeSagaWithFork)
+function* fetchSaga(){
+  // const { user } = yield take(SET_CURRENT_USER);
+  // console.log('user ',user);
+  // const { id } = user;
+
+  //https://jsonplaceholder.typicode.com/
+  const response = yield fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+  const items = yield response.json();
+  console.log('items : ',items);
+}
+
+// fork
+
+// sagaMiddleware.run(takeSaga)
+// sagaMiddleware.run(takeEverySaga)
+// sagaMiddleware.run(takeSagaWithFork)
+
+sagaMiddleware.run(fetchSaga)
 
 const main=async ()=>{
-    store.dispatch({type: 'testTake'})
-    store.dispatch({type: 'testTake'})
-    store.dispatch({type: 'testTakeEvery'})
-    store.dispatch({type: 'testTakeEvery'})
-    store.dispatch({type: 'testTakeWithFork'})
-    store.dispatch({type: 'testTakeWithFork'})
+    // store.dispatch({type: 'testTake'});
+    // console.log('wait 5sec')
+    // wait(5000)
+    // store.dispatch({type: 'testTake'})
+    // store.dispatch({type: 'testTakeEvery'})
+    // store.dispatch({type: 'testTakeEvery'})
+    // store.dispatch({type: 'testTakeWithFork'})
+    // store.dispatch({type: 'testTakeWithFork'})
 }
 
 main();
